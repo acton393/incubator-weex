e1f73b6f93878d2ea0ad41a1fe82a174bae6a221
acton393 <zhangxing610321@gmail.com>
2017-12-14 19:12:34 +0800

    * [iOS] release jsContext object

--- ios/sdk/WeexSDK/Sources/Bridge/WXBridgeContext.m
+++ ios/sdk/WeexSDK/Sources/Bridge/WXBridgeContext.m
@@ -462,9 +462,10 @@ _Pragma("clang diagnostic pop") \
             classDefinition.attributes = kJSClassAttributeNoAutomaticPrototype;
             JSClassRef globalObjectClass = JSClassCreate(&classDefinition);
             JSGlobalContextRef sandboxGlobalContextRef = JSGlobalContextCreateInGroup(contextGroup, globalObjectClass);
-            JSObjectRef sandBoxGlobalObjectRef = JSContextGetGlobalObject(sandboxGlobalContextRef);
-            JSObjectSetPrototype(sandboxGlobalContextRef, sandBoxGlobalObjectRef, [instanceContextEnvironment JSValueRef]);
+            JSClassRelease(globalObjectClass);
             sdkInstance.instanceJavaScriptContext = [JSContext contextWithJSGlobalContextRef:sandboxGlobalContextRef];
+            JSGlobalContextRelease(sandboxGlobalContextRef);
+            JSObjectSetPrototype(sdkInstance.instanceJavaScriptContext.JSGlobalContextRef,JSContextGetGlobalObject(sdkInstance.instanceJavaScriptContext.JSGlobalContextRef), [instanceContextEnvironment JSValueRef]);
             [sdkInstance.instanceJavaScriptContext evaluateScript:jsBundleString];
         }];
     } else {
@@ -499,8 +500,6 @@ _Pragma("clang diagnostic pop") \
     sdkInstance.instanceJavaScriptContext = nil;
     
     [self callJSMethod:@"destroyInstance" args:@[instance]];
-    WXSDKInstance *sdkIntance = [WXSDKManager instanceForID:instance];
-    sdkIntance.instanceJavaScriptContext = NULL;
 }
 
 - (void)forceGarbageCollection
