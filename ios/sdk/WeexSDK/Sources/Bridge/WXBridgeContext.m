e3a909ee354cd9c5e262108edb304aa08d016925
acton393 <zhangxing610321@gmail.com>
2018-01-08 19:48:58 +0800

    * [iOS] callJS on instanceContext

--- ios/sdk/WeexSDK/Sources/Bridge/WXBridgeContext.m
+++ ios/sdk/WeexSDK/Sources/Bridge/WXBridgeContext.m
@@ -782,7 +782,12 @@ _Pragma("clang diagnostic pop") \
     }
     
     if ([tasks count] > 0 && execIns) {
-        [self callJSMethod:@"callJS" args:@[execIns, tasks]];
+        WXSDKInstance * execInstance = [WXSDKManager instanceForID:execIns];
+        if (execInstance.instanceJavaScriptContext) {
+            [self callJSMethod:@"__WEEX_CALL_JAVASCRIPT__" args:@[execIns, tasks] onContext:execInstance.instanceJavaScriptContext completion:nil];
+        } else {
+            [self callJSMethod:@"callJS" args:@[execIns, tasks]];
+        }
     }
     
     if (hasTask) {
